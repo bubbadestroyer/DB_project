66111fdeb31b8945537d3ef125e8b0e2e157be86
Egor <godenzo@vk.com>
2022-08-03 16:09:10 +0300

    Added get_date & get_columns name functions

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from mysql.connector import Error
+from config import config
 
 def connect_to_sql(hostname, username, password, name):
     try:
@@ -11,3 +12,28 @@ def connect_to_sql(hostname, username, password, name):
     except Error as err:
         print(f'Что-то пошло не так: {err}')
     return connection_db
+
+
+def get_columns_name():
+    cursor.execute('''SHOW COLUMNS FROM costs''')
+    result = cursor.fetchall()
+    list = [row[0] for row in result]
+    return list
+
+def get_date():
+    cursor.execute('''SELECT * FROM costs''')
+    result = cursor.fetchall()
+    lst = []
+    print(result)
+    for row in result:
+        for rows in row:
+            lst.append(str(rows))
+    lst = [tuple(lst)]
+    return lst
+        
+        
+conn = connect_to_sql(config['host'], config['username'], config['password'],
+                      config['name_database'])
+cursor = conn.cursor()
+
+get_date()
