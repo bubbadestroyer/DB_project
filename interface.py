ba4158bf148377ea395a6ec57afeb2359279ff62
Egor <godenzo@vk.com>
2022-08-06 18:10:44 +0300

    Added get_data func

--- interface.py
+++ interface.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import Button, ttk
+from unicodedata import category
 from database import *
 from tkcalendar import DateEntry, Calendar
 
@@ -36,8 +37,7 @@ class TableFrame(tk.Frame):
             table.column(header, width=150)
 
         for row in get_costs():
-            print(row)
-            table.insert('', 0, values=row)
+            table.insert('', tk.END, values=row)
 
         scroll_pane = ttk.Scrollbar(self, command=table.yview)
         table.configure(yscrollcommand=scroll_pane.set)
@@ -51,6 +51,13 @@ class DataFrame(tk.Frame):
         super().__init__(parent)
         self.put_widges()
 
+    def get_data(self):
+        amount = self.amount_label_value.get()
+        date = self.date_label_value.get()
+        date = f'{date[6:10]}-{date[3:5]}-{date[:2]}'
+        category_id = self.category_label_value.get()
+        insert_data(amount, date, get_categories_from_category(category_id))
+
     def put_widges(self):
         self.amount_label_text = ttk.Label(self, text='Сумма')
         self.amount_label_value = ttk.Entry(self, justify=tk.RIGHT)
@@ -58,13 +65,39 @@ class DataFrame(tk.Frame):
         self.date_label_value = DateEntry(self)
         self.category_label_text = ttk.Label(self, text='Категория')
         self.category_label_value = ttk.Combobox(self, values=get_categories())
+        self.btn_send = ttk.Button(self,
+                                   text='Отправить',
+                                   command=self.get_data)
 
-        self.amount_label_text.grid(row=0, column=0, sticky='w', cnf=self.master.conf)
-        self.amount_label_value.grid(row=0, column=1, sticky='e', cnf=self.master.conf)
-        self.date_label_text.grid(row=1, column=0, sticky='w', cnf=self.master.conf)
-        self.date_label_value.grid(row=1, column=1, sticky='e', cnf=self.master.conf)
-        self.category_label_text.grid(row=2, column=0, sticky='w', cnf=self.master.conf)
-        self.category_label_value.grid(row=2, column=1, sticky='e', cnf=self.master.conf)
+        self.amount_label_text.grid(row=0,
+                                    column=0,
+                                    sticky='w',
+                                    cnf=self.master.conf)
+        self.amount_label_value.grid(row=0,
+                                     column=1,
+                                     sticky='e',
+                                     cnf=self.master.conf)
+        self.date_label_text.grid(row=1,
+                                  column=0,
+                                  sticky='w',
+                                  cnf=self.master.conf)
+        self.date_label_value.grid(row=1,
+                                   column=1,
+                                   sticky='e',
+                                   cnf=self.master.conf)
+        self.category_label_text.grid(row=2,
+                                      column=0,
+                                      sticky='w',
+                                      cnf=self.master.conf)
+        self.category_label_value.grid(row=2,
+                                       column=1,
+                                       sticky='e',
+                                       cnf=self.master.conf)
+        self.btn_send.grid(row=3,
+                           column=0,
+                           columnspan=2,
+                           sticky='n',
+                           cnf=self.master.conf)
 
 
 app = App()
